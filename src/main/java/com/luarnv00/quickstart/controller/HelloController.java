517d2ac6ea8dada766ddf6dce186bec3203c8d88
luan ramos vicente <luanrv00@gmail.com>
2024-09-27 23:26:14 -0300

    chore: remove unused ResponseBody annotation

--- src/main/java/com/luarnv00/quickstart/controller/HelloController.java
+++ src/main/java/com/luarnv00/quickstart/controller/HelloController.java
@@ -2,14 +2,14 @@ package com.luarnv00.quickstart.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public @ResponseBody String index(@RequestParam(value = "name", defaultValue = "world") String name) {
+    public String index(@RequestParam(value = "name", defaultValue = "world", required = false) String name) {
         return String.format("hello %s", name);
     }
 }
