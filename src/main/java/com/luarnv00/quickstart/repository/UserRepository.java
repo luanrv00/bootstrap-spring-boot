bea6a50f451cc6e75da96bd805790cdfbc9c482e
luan ramos vicente <luanrv00@gmail.com>
2024-10-04 09:43:22 -0300

    setup: connect springboot and mysql containers

--- src/main/java/com/luarnv00/quickstart/repository/UserRepository.java
+++ src/main/java/com/luarnv00/quickstart/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package com.luarnv00.quickstart.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.luarnv00.quickstart.model.User;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
 }
